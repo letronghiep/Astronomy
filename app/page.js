04d640eeab898a0d6e1cecdbb87f9e6235f3dc83
letronghiep <letronghiep213@gmail.com>
2023-09-24 17:59:51 +0700

    add login btn

--- app/page.js
+++ app/page.js
@@ -1,10 +1,15 @@
+'use client'
 import Image from 'next/image'
 import LoginPage from './(auth)/login/page'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main>
       {/* <LoginPage /> */}
+      <Link href="/login" className='btn btn-primary btn-sm'>
+        Login
+      </Link>
     </main>
   )
 }
